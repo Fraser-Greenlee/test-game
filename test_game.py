a44321ff7cc399bd957e50c4b2f87f912fc04273
Fraser Greenlee <fraser.greenlee@icloud.com>
2022-12-28 16:21:20 +0000

    .

--- test_game.py
+++ test_game.py
@@ -12,7 +12,7 @@ class App:
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.text(55, 41, "Hello, Pyxel!", pyxel.frame_count % 16)
+        pyxel.text(55, 41, "Hello, World!", pyxel.frame_count % 16)
         pyxel.blt(61, 66, 0, 0, 0, 38, 16)
 
 
